f101ef8195a491aff4fddda747d155afe95c404c
mahbub1600 <ju.mahbub@gmail.com>
2020-09-13 02:20:23 +0600

    Updates

---
+++ precache-manifest.3eab16e0517895f636921b0ca9526cc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b92224b4723d1f19cbc186c37e67771",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98a8d2033d7def477e4d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fc1e535031f22a6c0e29",
+    "url": "/static/css/main.b982ee95.chunk.css"
+  },
+  {
+    "revision": "98a8d2033d7def477e4d",
+    "url": "/static/js/2.40bc1a45.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.40bc1a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1e535031f22a6c0e29",
+    "url": "/static/js/main.b0e0f024.chunk.js"
+  },
+  {
+    "revision": "b566212e65b7f09bb2b8",
+    "url": "/static/js/runtime-main.63c35337.js"
+  }
+]);
